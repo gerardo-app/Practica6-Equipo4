fe1bd30489a9ce57972516a75ee2573d8c904f18
DianaBRA <85389615+DianaBRA@users.noreply.github.com>
2021-08-09 17:17:52 -0500

    Update Nexus_config.py
    CODIGO DEL_ ARCHIVO Nexus_config.py

--- Nexus_config.py
+++ Nexus_config.py
@@ -1,18 +1,18 @@
 from nornir import InitNornir
-from normir_utils.plugins.functions import print_result
+from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks import netmiko_send_config, netmiko_send_command
 
 nr = InitNornir(
 	config_file="config.yaml",  dry_run=True
 	)
 	# open NX-OS Nexus 3000 and 9000 family of swithches
-	next1=nr.run(netmiko_send_config, config_file = "Nexus")
-	next2=nr.run(netmiko_send_config, config_file = "vlan")
-	next3=nr.run(netmiko_send_config, config_commands = ["Interface lo 6","description config con Nornir 3.1.0","ip add 6.6.6.6/32"])
-	next4=nr.run(netmiko_send_config, config_string = "show ip int brief")
-	next5=nr.run(netmiko_send_config, config_string = "show vlan b")
-	print_result(Next1)
-	print_result(Next2)
-	print_result(Next3)
-	print_result(Next4)
-	print_result(Next5)
+	Nex1=nr.run(netmiko_send_config, config_file = "Nexus")
+	Nex2=nr.run(netmiko_send_config, config_file = "vlan")
+	Nex3=nr.run(netmiko_send_config, config_commands = ["Interface lo 6","description config con Nornir 3.1.0","ip add 6.6.6.6/32"])
+	Nex4=nr.run(netmiko_send_config, config_string = "show ip int brief")
+	Nex5=nr.run(netmiko_send_config, config_string = "show vlan b")
+	print_result(Nex1)
+	print_result(NeX2)
+	print_result(Nex3)
+	print_result(Nex4)
+	print_result(Nex5)
