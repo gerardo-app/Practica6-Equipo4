38e6b89434e707c569b8325cc899c8f1b8788a19
DianaBRA <85389615+DianaBRA@users.noreply.github.com>
2021-08-09 17:20:31 -0500

    Update Nexus_config.py

--- Nexus_config.py
+++ Nexus_config.py
@@ -12,7 +12,7 @@ nr = InitNornir(
 	Nex4=nr.run(netmiko_send_config, config_string = "show ip int brief")
 	Nex5=nr.run(netmiko_send_config, config_string = "show vlan b")
 	print_result(Nex1)
-	print_result(NeX2)
+	print_result(Nex2)
 	print_result(Nex3)
 	print_result(Nex4)
 	print_result(Nex5)
